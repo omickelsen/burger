189ab05907b907ba4c6b59f0234e5ea1247093ef
omickelsen <omickelsen@gmail.com>
2019-05-13 12:25:24 -0600

    adding jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,10 @@ var mysql = require('mysql');
 
 // Create the MySQL connection object
 var connection;
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else
 
 {
 	// DB is local on localhost
