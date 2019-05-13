9dbdbfb5ab1ec846f0d5081a0626dcdecd6231f6
omickelsen <omickelsen@gmail.com>
2019-05-13 12:05:39 -0600

    fixed random bugs

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,7 @@ var mysql = require('mysql');
 // Create the MySQL connection object
 var connection;
 
-if (process.env.JAWSDB_URL) {
-	// DB is JawsDB on Heroku
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+{
 	// DB is local on localhost
 	connection = mysql.createConnection({
 		port: 3306,
--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
     
 -- Create the burgers_db database --
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
